ab5271b594f6edbe31f503168392e022d86d8efd
BenjaminE <benjamin.ehnes@fh-erfurt.de>
2022-08-29 09:36:09 +0200

    fixed indentation

--- Templates/WebAPI/Devon4Net.Application.WebAPI.Implementation/Business/DishManagement/Dto/CategoryDto.cs
+++ Templates/WebAPI/Devon4Net.Application.WebAPI.Implementation/Business/DishManagement/Dto/CategoryDto.cs
@@ -2,7 +2,7 @@ using Newtonsoft.Json;
 
 namespace Devon4Net.Application.WebAPI.Implementation.Business.DishManagement.Dto
 {
-public class CategoryDto
+    public class CategoryDto
     {
         [JsonProperty(PropertyName = "id")]
         public long Id { get; set; }
